ce2ad6f104e7940015a760f2ca3063765b4be09f
Vanson Wing Leung <vanson@vanportdev.com>
2023-09-20 10:52:25 +0800

    Initial Commit

---
+++ AsyncLazyVStackAnim/AsyncLazyVStackAnimApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncLazyVStackAnimApp.swift
+//  AsyncLazyVStackAnim
+//
+//  Created by Vanson Leung on 20/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncLazyVStackAnimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
